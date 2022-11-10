9f380c996b02f509b50eeadf12f8ab5070818a60
medorio <20193l001073@utcv.edu.mx>
2022-11-10 22:34:59 +0000

    Se cambio el puerto 8080 a 5050

--- sample-app.sh
+++ sample-app.sh
@@ -18,5 +18,5 @@ echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a 
